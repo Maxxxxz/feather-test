b915f9a2144991229cbb91fc8480f6b205ee7f36
Michael Cooper <mc454916@ohio.edu>
2021-01-22 17:43:28 -0500

    add wifiparameters

--- featehr_test.ino
+++ featehr_test.ino
@@ -1,36 +1,57 @@
 #include <stdio.h>
-#include <HX711.h>
+//#include <HX711.h>
 
 // to install
 //https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/using-arduino-ide
-//#include <WiFiManager.h>
+#include <WiFiManager.h>
 
-HX711 cell;
-const int CELL_D_PIN = 12;
-const int CELL_SCK_PIN = 14;
+//HX711 cell;
+//const int CELL_D_PIN = 12;
+//const int CELL_SCK_PIN = 14;
 
 //const long LOADCELL_OFFSET = 50682624;
 //const long LOADCELL_DIVIDER = 5895655;
+
+// what? https://github.com/tzapu/WiFiManager/issues/379
+WiFiManagerParameter ownerEmail("email", "E-Mail", "");
+WiFiManagerParameter ownerPassword("password", "Password", "");
+
+WiFiManager wfm;
 
 void setup() {
   Serial.begin(9600);
   delay(100);
   Serial.println("Hello, World!");
-//  WiFiManager wfm;
-//  wfm.autoConnect("NAME", "PASSWORD");
 
-  cell.begin(CELL_D_PIN, CELL_SCK_PIN);
+  wfm.disconnect();
+  
+  // Testing; always start in station mode
+  WiFi.mode(WIFI_STA);
+
+  // Add parameters here
+
+  wfm.addParameter(&ownerEmail);
+  wfm.addParameter(&ownerPassword);
+
+  wfm.setDarkMode(true);
+//  wfm.autoConnect("feather", "PASSWORD");
+  wfm.startConfigPortal("feather", "PASSWORD");
+
+//  cell.begin(CELL_D_PIN, CELL_SCK_PIN);
 //  cell.set_cell(LOADCELL_DIVIDER);
 //  cell.set_offset(LOADCELL_OFFSET);
 }
 
 void loop() {
-  if (cell.is_ready()) {
-    long reading = cell.read();
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
-  } else {
-    Serial.println("HX711 not found.");
-  }
+//  if (cell.is_ready()) {
+//    long reading = cell.read();
+//    Serial.print("HX711 reading: ");
+//    Serial.println(reading);
+//  } else {
+//    Serial.println("HX711 not found.");
+//  }
+
+  Serial.println("Hello!");
+
   delay(1000);
 }
